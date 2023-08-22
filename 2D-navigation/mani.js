fcba20efc3bf243476ed9fa91db0f19cb9f3c6ef
Arjunkumar01k <111638147+Arjunkumar01k@users.noreply.github.com>
2023-08-22 23:33:45 +0530

    Add files via upload

---
+++ 2D-navigation/mani.js
@@ -0,0 +1,415 @@
+
+// start 
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Examples of architectural tools") {
+      x.innerHTML = "Different Types of Architect Tools and Equipment In our architecture equipment list below, you will find: 1. Architecture instruments  2. Architecture drawing tools  3. Architecture drafting tools ";
+    } else {
+      x.innerHTML = "Examples of architectural tools";
+    }
+  }
+
+
+
+
+
+// start centralized Applicaton process
+function myNsp() {
+    var x = document.getElementById("nsp");
+    if (x.innerHTML === "The National Scholarships Portal (NSP) ") {
+      x.innerHTML = "The National Scholarships Portal (NSP) is a digital platform introduced by the Government of India to streamline the scholarship application and disbursement process for various centrally sponsored scholarships.  It aims to provide financial assistance to meritorious and economically disadvantaged students pursuing higher education in India. By centralizing the scholarship application process, the NSP simplifies the procedure for students, making it easier for them to apply for multiple scholarships through a single online platform.";
+    } else {
+      x.innerHTML = "The National Scholarships Portal (NSP) ";
+    }
+  }
+
+  // end centralized Applicaton process
+
+
+
+
+
+// start centralized Applicaton process
+function myFunction() {
+    var x = document.getElementById("centralized");
+    if (x.innerHTML === "Centralized Application Process") {
+      x.innerHTML = "Centralized Application Process: The NSP provides a single platform for students to apply for various scholarships offered by different governments.";
+    } else {
+      x.innerHTML = "Centralized Application Process";
+    }
+  }
+
+  // end centralized Applicaton process
+
+
+
+// start Multiple Scholarship Schemes
+function myMultiple() {
+    var x = document.getElementById("multiple");
+    if (x.innerHTML === "Multiple Scholarship Schemes") {
+      x.innerHTML = "Multiple Scholarship Schemes: The portal covers a wide range of scholarship schemes, including pre-matric, post-matric, and merit-based scholarships, catering to students from different academic backgrounds and levels of education.";
+    } else {
+      x.innerHTML = "Multiple Scholarship Schemes";
+    }
+  }
+
+  // end Multiple Scholarship Schemes
+
+
+
+
+
+// start Transparency and Accountability
+function myTransparency() {
+    var x = document.getElementById("transparency");
+    if (x.innerHTML === "Transparency and Accountability") {
+      x.innerHTML = "Transparency and Accountability: The NSP ensures transparency in the scholarship process by providing real-time updates on the status of applications, disbursement details, and scholarship amounts. It helps students track their applications and know the outcome promptly.     ";
+    } else {
+      x.innerHTML = "Transparency and Accountability";
+    }
+  }
+
+  // end Transparency and Accountability
+
+
+
+
+// start Timely Disbursement
+function myDisbursement() {
+    var x = document.getElementById("disbursement");
+    if (x.innerHTML === "Timely Disbursement") {
+      x.innerHTML = "Timely Disbursement: With a streamlined online application process, the NSP aims to ensure timely disbursal of scholarship funds directly into the bank accounts of eligible students, reducing delays and paperwork.";
+    } else {
+      x.innerHTML = "Timely Disbursement";
+    }
+  }
+
+  // end Timely Disbursement
+
+
+
+
+
+// start User-Friendly Interface
+function myInterface() {
+    var x = document.getElementById("interface");
+    if (x.innerHTML === "User-Friendly Interface") {
+      x.innerHTML = "User-Friendly Interface: The portal is designed to be user-friendly and accessible, making it easy for students to navigate, register, and apply for scholarships without any technical hassles.";
+    } else {
+      x.innerHTML = "User-Friendly Interface";
+    }
+  }
+
+  // end User-Friendly Interface
+
+
+
+
+
+// start Mobile Application
+function myApplication() {
+    var x = document.getElementById("application");
+    if (x.innerHTML === "Mobile Application") {
+      x.innerHTML = "Mobile Application: The NSP offers a mobile application, enabling students to access the portal and apply for scholarships using their smartphones, increasing accessibility, especially in rural areas. ";
+    } else {
+      x.innerHTML = "Mobile Application";
+    }
+  }
+
+  // end Mobile Application
+
+
+
+
+
+// start Transparency and Accountability
+function myAuthorities() {
+    var x = document.getElementById("authorities");
+    if (x.innerHTML === "Dashboards for Authorities") {
+      x.innerHTML = "Dashboards for Authorities: Apart from students, the NSP provides dedicated dashboards for institutions and state authorities to manage scholarship applications, verify documents, and facilitate the disbursement process. ";
+    } else {
+      x.innerHTML = "Dashboards for Authorities";
+    }
+  }
+
+  // end Transparency and Accountability
+
+
+
+
+
+// start Digitization of Documents
+function myDigitization() {
+    var x = document.getElementById("digitization");
+    if (x.innerHTML === "Digitization of Documents") {
+      x.innerHTML = "Digitization of Documents: The portal allows students to upload and submit their documents online, reducing the need for physical paperwork and making the process more eco-friendly. ";
+    } else {
+      x.innerHTML = "Digitization of Documents";
+    }
+  }
+
+  // end Digitization of Documents
+
+
+
+
+// start Transparency and Accountability
+function mySolution() {
+    var x = document.getElementById("solution");
+    if (x.innerHTML === "Solution Statement") {
+      x.innerHTML = "CI/CD is a modern DevOps practice that involves automating the build, testing, and deployment of code changes. Here are some key benefits of implementing CI/CD at the client setup";
+    } else {
+      x.innerHTML = "Solution Statement";
+    }
+  }
+
+  // end Transparency and Accountability
+
+
+
+// start Faster and More Frequent Releases
+function myFaster() {
+    var x = document.getElementById("faster");
+    if (x.innerHTML === "Faster and More Frequent Releases") {
+      x.innerHTML = "Faster and More Frequent Releases: CI/CD pipelines automate the build, test, and deployment processes, allowing developers to release new features and updates more frequently and with greater confidence. This leads to faster delivery of software to end-users and a competitive edge in the market.";
+    } else {
+      x.innerHTML = "Faster and More Frequent Releases";
+    }
+  }
+
+  // end Faster and More Frequent Releases
+
+
+
+// start Improved Code Quality
+function myImproved() {
+    var x = document.getElementById("improved");
+    if (x.innerHTML === "Improved Code Quality") {
+      x.innerHTML = "Improved Code Quality: With automated testing in CI/CD pipelines, code changes are thoroughly tested before deployment. This helps catch bugs and issues early in the development process, leading to higher code quality and more stable releases.      ";
+    } else {
+      x.innerHTML = "Improved Code Quality";
+    }
+  }
+
+  // end Improved Code Quality
+
+
+
+
+
+// start Reduced Manual Errors
+function myReduced() {
+    var x = document.getElementById("reduced");
+    if (x.innerHTML === "Reduced Manual Errors") {
+      x.innerHTML = "Reduced Manual Errors: Automating the build and deployment process reduces the chances of human errors that might occur when performing these tasks manually. This leads to more reliable and consistent releases.";
+    } else {
+      x.innerHTML = "Reduced Manual Errors";
+    }
+  }
+
+  // end Reduced Manual Errors
+
+
+
+// start Reduced Manual Errors
+function myIncreased() {
+  var x = document.getElementById("increased");
+  if (x.innerHTML === " Increased Collaboration") {
+    x.innerHTML = "Increased Collaboration: CI/CD encourages collaboration between developers, testers, and operations teams. The automated pipeline provides visibility into the status of builds and deployments, fostering better communication and coordination among team members.";
+  } else {
+    x.innerHTML = " Increased Collaboration";
+  }
+}
+
+// end Reduced Manual Errors
+
+
+
+
+// start Time and Cost Savings
+function myTimecost() {
+  var x = document.getElementById("timecost");
+  if (x.innerHTML === " Time and Cost Savings") {
+    x.innerHTML = "Time and Cost Savings: By automating repetitive tasks, CI/CD reduces the time and effort required for manual processes, saving both time and costs associated with development and deployment.";
+  } else {
+    x.innerHTML = " Time and Cost Savings";
+  }
+}
+
+// end Time and Cost Savings
+
+
+
+
+
+// start Faster and More Frequent Releases
+function myFaster() {
+    var x = document.getElementById("faster");
+    if (x.innerHTML === "Faster and More Frequent Releases") {
+      x.innerHTML = "Faster and More Frequent Releases: CI/CD pipelines automate the build, test, and deployment processes, allowing developers to release new features and updates more frequently and with greater confidence. This leads to faster delivery of software to end-users and a competitive edge in the market.";
+    } else {
+      x.innerHTML = "Faster and More Frequent Releases";
+    }
+  }
+
+  // end Faster and More Frequent Releases
+
+
+
+
+// start Faster and More Frequent Releases
+function myFaster() {
+    var x = document.getElementById("faster");
+    if (x.innerHTML === "Faster and More Frequent Releases") {
+      x.innerHTML = " Faster and More Frequent Releases: CI/CD pipelines automate the build, test, and deployment processes, allowing developers to release new features and updates more frequently and with greater confidence. This leads to faster delivery of software to end-users and a competitive edge in the market.";
+    } else {
+      x.innerHTML = "Faster and More Frequent Releases";
+    }
+  }
+
+  // end Faster and More Frequent Releases
+ 
+
+// start Simplified Application Deployment
+function mySimplified() {
+    var x = document.getElementById("simplified");
+    if (x.innerHTML === "Simplified Application Deployment") {
+      x.innerHTML = " Simplified Application Deployment: OCP abstracts the complexities of container orchestration and provides a streamlined way to deploy applications in containers. It automates the deployment process, making it easier and faster to bring applications into production.";
+    } else {
+      x.innerHTML = "Simplified Application Deployment";
+    }
+  }
+
+  // end Simplified Application Deployment
+ 
+
+// start scalability
+function myScalability() {
+    var x = document.getElementById("scalability");
+    if (x.innerHTML === "Scalability") {
+      x.innerHTML = "Scalability: OCP allows applications to scale both vertically and horizontally. It can automatically scale applications based on demand, ensuring optimal performance and resource utilization.";
+    } else {
+      x.innerHTML = "Scalability";
+    }
+  }
+
+  // end scalability
+ 
+
+
+  function myContainer(){
+    var x = document.getElementById("container");
+    if(x.innerHTML === "Container Management"){
+        x.innerHTML = "Container Management: OCP provides tools to manage container lifecycles, including rolling updates, rollbacks, and automatic healing of containers. This ensures high availability and reduces downtime during updates or failures.";
+    }
+    else{
+        x.innerHTML = "Container Management";
+    }
+  }
+
+
+
+
+
+// start DevOps Enablement
+function myDevopsenbl() {
+    var x = document.getElementById("devenbl");
+    if (x.innerHTML === "DevOps Enablement") {
+      x.innerHTML = "DevOps Enablement: OCP fosters collaboration between development and operations teams by providing a shared platform and consistent environments for building, testing, and deploying applications. This accelerates the DevOps process.";
+    } else {
+      x.innerHTML = "DevOps Enablement";
+    }
+  }
+
+  // end DevOps Enablement
+
+
+
+
+// start scalability
+function myApplporti() {
+    var x = document.getElementById("applporti");
+    if (x.innerHTML === "Application Portability") {
+      x.innerHTML = "Application Portability: Containers deployed on OCP are highly portable. Developers can build once and deploy anywhere, whether it's on the local development environment, staging environment, or production environment.";
+    } else {
+      x.innerHTML = "Application Portability";
+    }
+  }
+
+  // end scalability
+
+
+
+function myMonitoring(){
+    var x = document.getElementById("monitoring");
+    if(x.innerHTML === "Monitoring and Logging"){
+        x.innerHTML = "Monitoring and Logging: OCP offers built-in monitoring and logging capabilities to gain insights into application performance and troubleshoot issues effectively.      ";
+    }else {
+        x.innerHTML = "Monitoring and Logging";
+    }
+}
+
+
+
+
+  //start problem statement 
+
+  function mypFunction() {
+    var demoElement = document.getElementById("demo");
+    
+    if (demoElement.style.display === "none") {
+      demoElement.style.display = "block"; // Show the element
+    } else {
+      demoElement.style.display = "none";  // Hide the element again
+    }
+  }
+
+  // end problem statement
+  function myMonitoringsetup() {
+    var setupElement = document.getElementById("monitoringsetup");
+  
+    if (setupElement.style.display === "none") {
+      setupElement.style.display = "block";
+    } else {
+      setupElement.style.display = "none";
+    }
+  }
+  
+
+
+
+
+
+// start PostgreSql setup upgrade latest version
+function myPostgre() {
+  var x = document.getElementById("postgresqlset");
+  if (x.innerHTML === "PostgreSQL setup upgrade latest version") {
+    x.innerHTML = "    PostgreSQL, often referred to as Postgres, is a powerful open-source relational database management system (RDBMS). It is known for its advanced features, extensibility, and compliance with SQL standards. PostgreSQL supports a wide range of data types, indexing methods, and procedural languages, making it a popular choice for various applications, from small-scale projects to large-scale enterprise systems.";
+  } else {
+    x.innerHTML = "PostgreSQL setup upgrade latest version";
+  }
+}
+
+// end PostgreSql setup upgrade latest version
+
+  
+
+
+
+//   var heading = document.createElement("h1");
+//   heading.textContent = "Hello, JavaScript and HTML!";
+//   heading.style.color = "blue";
+//   heading.style.fontFamily = "time new romane";
+
+
+
+//   // Create a <p> element
+//   var paragraph = document.createElement("p");
+//   paragraph.textContent = "This is a dynamically created paragraph.";
+
+//   // Append the elements to the container
+//   var container = document.getElementById("container");
+//   container.appendChild(heading);
+//   container.appendChild(paragraph);
